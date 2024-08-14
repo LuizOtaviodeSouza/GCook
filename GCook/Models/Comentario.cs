d6186160db2f23add603f9b2daf022d2ef0f6295
Luiz <lui-otavio@hotmail.com>
2024-08-14 14:33:54 -0300

    dfsg

---
+++ GCook/Models/Comentario.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GCook.Models;
+
+[Table("Comentario")]
+    public class Comentario
+    {
+        [Key]
+        public int ReceitaID { get; set; }
+
+        [ForeignKey("ReceitaId")]
+        public Receita Receita { get; set; }
+
+        [Required]
+        public string UsuarioId { get; set;}
+
+        [ForeignKey("UsuarioId")]
+        public Usuario Usuario { get; set; }
+
+        public DateTime DataComentario { get; set;}
+
+        [StringLength(300)]
+        public string TextoComentario { get; set; }
+        
+
+    }
